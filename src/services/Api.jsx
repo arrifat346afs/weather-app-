e05125fbc700f0798af06715347cfb4557a60467
arrifat346afs <arrifat1235@gmail.com>
2025-02-20 20:39:45 +0600

    Auto commit

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const URL = `http://api.weatherapi.com/v1`
 
 const Api = () => {
+
+    const [temp, setTemp] = useState(second)
+
+    useEffect(()=>{
+        const fatchData= async ()=>{
+            const result = await fetch(URL)
+            console.log(result)
+        }
+        fatchData()
+    },[])
+
   return (
     <div>
       <h1>hi</h1>
