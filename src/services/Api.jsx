d677124d7787eeed81a7a0f2d134b7e0463526fa
arrifat346afs <arrifat1235@gmail.com>
2025-02-20 20:54:51 +0600

    Auto commit

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -3,13 +3,13 @@ import React, { useEffect } from "react";
 const URL = `http://api.weatherapi.com/v1/current.json?key=911c12b46fee4dba8c5133138252002&q=Dhaka`;
 
 const Api = () => {
-  //   const [temp, setTemp] = useState("");
+    const [temp, setTemp] = useState(0);
 
   useEffect(() => {
     const fatchData = async () => {
       const result = await fetch(URL);
       result.json().then((json) => {
-        console.log(json);
+        setTemp(json.current.temp_f)
       });
     };
     fatchData();
@@ -17,7 +17,7 @@ const Api = () => {
 
   return (
     <div>
-      <h1>hi</h1>
+      <h1>Dhaka Temp now: {temp}</h1>
     </div>
   );
 };
