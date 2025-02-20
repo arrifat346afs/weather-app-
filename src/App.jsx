3f54fc643a4aee7ded6ebeaf3c257b0bd8009bee
arrifat346afs <arrifat1235@gmail.com>
2025-02-20 22:12:59 +0600

    Auto commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-
-
-import './App.css'
-
+import "./App.css";
+import Scarch from "./component/Scarch";
 
 const API_KEY = "911c12b46fee4dba8c5133138252002";
 
-const BASE_URL ="http://api.weatherapi.com/v1"
+const BASE_URL = "http://api.weatherapi.com/v1";
 
 function App() {
-
-
   return (
     <>
-     
+      <Scarch />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/component/Scarch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Scarch = () => {
+  return (
+    <div>
+      THis id you copilet
+    </div>
+  )
+}
+
+export default Scarch
