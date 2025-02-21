338595ede0f33cff6eaf286abeb8789d004e48e5
arrifat346afs <arrifat1235@gmail.com>
2025-02-21 10:49:32 +0600

    feat(search): Add padding to search input

--- src/component/Scarch.jsx
+++ src/component/Scarch.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Scarch = () => {
   return (
     <div>
-     <input type="text" className="w-100 h-8 bg-[#815d67]  rounded-md focus:drop-shadow-xl"/>
+     <input type="text" className="w-100 h-8 bg-[#815d67] p-3 rounded-md focus:drop-shadow-xl"/>
     </div>
   )
 }
