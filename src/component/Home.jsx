a2217439c8b812d09c4cde74ad779946252f5c9f
arrifat346afs <arrifat1235@gmail.com>
2025-02-21 21:42:31 +0600

    feat(home): implement basic weather search functionality

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,37 +2,33 @@ import React from "react";
 
 import { useState } from "react";
 
-
-
-
 const api = {
-  key:"911c12b46fee4dba8c5133138252002",
+  key: "911c12b46fee4dba8c5133138252002",
   url: "http://api.weatherapi.com/v1",
-}
-
+};
 
 const Home = () => {
   const [scarchTarm, setScarchTaem] = useState("");
 
-
-  const scarchPress= ()=>{
+  const scarchPress = () => {
     fetch(`${api.url}current.json?key=${api.key}q=${scarchTarm}`)
-    .then((res)=> res.json())
-    .then((result)=>{
-      console.log(result)
-    })
-  }
-
-
-
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
 
   return (
-        <div className="scarch">
-          <input type="text" value={scarchTarm} onChange={(e) => setScarchTaem(e.target.value)} />
-          <button type="submit" onClick={scarchPress}>
-            <FiSearch />
-          </button>
-        </div>
+    <div className="scarch">
+      <input
+        type="text"
+        value={scarchTarm}
+        onChange={(e) => setScarchTaem(e.target.value)}
+      />
+      <button type="submit" onClick={scarchPress}>
+        <FiSearch />
+      </button>
+    </div>
   );
 };
 
