bc75d9b0a9ba7276067757ce3d2e6ca662a2d741
arrifat346afs <arrifat1235@gmail.com>
2025-02-21 19:22:19 +0600

    fix(app): Corrected typo in useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./css/App.css";
 import Scarch from "./component/Scarch";
+import { useState } from "react";
 
 const API_KEY = "911c12b46fee4dba8c5133138252002";
 
 const BASE_URL = "http://api.weatherapi.com/v1";
 
 function App() {
-  const [first, setfirst] = uses
+  const [first, setfirst] = useState("")
   return (
     <>
       
