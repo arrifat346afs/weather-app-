268d215e49be69d47db87f0ec6307068691d4d20
arrifat346afs <arrifat1235@gmail.com>
2025-02-21 21:14:01 +0600

    fix(api): correct weather API base URL

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Scarch from "./component/Scarch";
 import { useState } from "react";
-import { keyframes } from "motion";
 
 
 
-const BASE_URL = ;
 
 const api = {
   key:"911c12b46fee4dba8c5133138252002",
-  url: 
+  url: "http://api.weatherapi.com/v1",
 }
 
 
