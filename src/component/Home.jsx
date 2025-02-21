53bc0b22711ee67416a2ae35b909f30f59ea3000
arrifat346afs <arrifat1235@gmail.com>
2025-02-21 22:17:22 +0600

    refactor(home): comment out weather data display

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -31,9 +31,9 @@ const Home = () => {
   return (
     <>
       <SearchBox searchTerm={searchTerm} setSearchTerm={setSearchTerm} searchPress={searchPress} />
-      <p>{weatherData.location.name}</p>
-      <p>{weatherData.current.temp_c}</p>
-      <img src={weatherData.current.condition.icon} alt="weather icon" />
+      {/* <p>{weatherData.location.name}</p> */}
+      {/* <p>{weatherData.current.temp_c}</p> */}
+      {/* <img src={weatherData.current.condition.icon} alt="weather icon" /> */}
     </>
   );
 };
