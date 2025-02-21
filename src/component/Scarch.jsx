cb3ff237090333b4c624c29870584dd66e29117b
arrifat346afs <arrifat1235@gmail.com>
2025-02-21 19:42:33 +0600

    feat: update 1 files (src/component/Scarch.jsx) at 13:42

--- src/component/Scarch.jsx
+++ src/component/Scarch.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { FiSearch } from "react-icons/fi";
 import "../css/scarch.css";
 
-const Scarch = ({scarchTarm,setScarchTaem}) => {
+const Scarch = ({ scarchTarm, setScarchTaem }) => {
   return (
     <div className="scarch">
-      <input type="text" onClick={(e)=>setScarchTaem(e.target.value)}/>
+      <input type="text" onClick={(e) => setScarchTaem(e.target.value)} />
       <button type="submit">
         <FiSearch />
       </button>
