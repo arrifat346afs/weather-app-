d80455609fed4a6e3bfc3ee71d6616640a74639a
arrifat346afs <arrifat1235@gmail.com>
2025-02-21 23:07:00 +0600

    feat(home): display search history

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -64,6 +64,15 @@ const Home = () => {
       ) : (
         <p>Enter a city to get the weather data.</p>
       )}
+      
+      <h3>Search History</h3>
+      <ul>
+        {searchHistory.map((item, index) => (
+          <li key={index}>
+            {item.location.name} - {item.current.temp_c}°C
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
