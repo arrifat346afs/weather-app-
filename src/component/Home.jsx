4a03991ca3c5e81391e05438bba03951ff3fa725
arrifat346afs <arrifat1235@gmail.com>
2025-02-21 23:17:30 +0600

    fix(home): Fix typo in fetchWeatherData

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -35,7 +35,9 @@ const Home = () => {
 
  const fetchWeatherData=(latitude, longitude)=>{
   fetch(`${api.url}/current.jsaon?key=${api.key}&q=${latitude},${longitude}`)
-  .then((res)=)
+  .then((res)=>{
+    if()
+  })
  }
 
 
