1abe2270bd24b20bf236f3127680f12e88016e0c
arrifat346afs <arrifat1235@gmail.com>
2025-02-21 10:46:30 +0600

    fix(search): reduce focus shadow intensity

--- src/component/Scarch.jsx
+++ src/component/Scarch.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Scarch = () => {
   return (
     <div>
-     <input type="text" className="w-100 h-8 bg-[#815d67] rounded-md focus:drop-shadow-2xl"/>
+     <input type="text" className="w-100 h-8 bg-[#815d67] rounded-md focus:drop-shadow-md"/>
     </div>
   )
 }
