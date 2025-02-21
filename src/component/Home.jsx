cd3e883c188a024046811273221bf31cb6fd49c4
arrifat346afs <arrifat1235@gmail.com>
2025-02-21 20:45:36 +0600

    feat(home): add weather API constants

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import Scarch from "./component/Scarch";
 import { useState } from "react";
+
+const API_KEY = "911c12b46fee4dba8c5133138252002";
+
+const BASE_URL = "http://api.weatherapi.com/v1";
+
+
+
 const Home = () => {
   const [scarchTarm, setScarchTaem] = useState("");
   return (
