5a2a51bb8cdb194d631eb1851b4e62826a2079c3
arrifat346afs <arrifat1235@gmail.com>
2025-02-21 20:21:34 +0600

    refactor: Remove unused API key and base URL

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import "./css/App.css";
 import Scarch from "./component/Scarch";
 import { useState } from "react";
 
-const API_KEY = "911c12b46fee4dba8c5133138252002";
-
-const BASE_URL = "http://api.weatherapi.com/v1";
 
 function App() {
   const [scarchTarm, setScarchTaem] = useState("")
