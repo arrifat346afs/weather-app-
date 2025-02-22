1c26fb592edbea42a28214b019b64618242e8331
arrifat346afs <arrifat1235@gmail.com>
2025-02-22 23:07:06 +0600

    fix(home): fix geocode API request with lat and long

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
       (position)=>{
         const { latitude, longitude } = position.coords;
 
-        fetch(`${api.geocodeUrl}?lat=`)
+        fetch(`${api.geocodeUrl}?lat=${latitude}&`)
       }
     )
   }, []);
