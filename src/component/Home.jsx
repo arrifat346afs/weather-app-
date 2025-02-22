2985eca1b8ebd6586826cf8a82935aa7af708210
arrifat346afs <arrifat1235@gmail.com>
2025-02-22 23:27:50 +0600

    fix(api): rename url to weatherUrl for clarity

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -6,7 +6,7 @@ import { data } from "motion/react-client";
 
 const api = {
   key: "911c12b46fee4dba8c5133138252002",
-  url: "http://api.weatherapi.com/v1",
+  weatherUrl: "http://api.weatherapi.com/v1",
   geocodeUrl: "https://nominatim.openstreetmap.org/reverse",
 };
 
