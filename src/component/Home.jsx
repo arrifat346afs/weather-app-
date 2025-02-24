93888279074edfc13ca87a5466f5cbb673d3220a
arrifat346afs <arrifat1235@gmail.com>
2025-02-24 11:27:44 +0600

    feat: update 1 files (src/component/Home.jsx) at 05:27

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBox from "./SearchBox";
 import WatherCard from "./WatherCard";
 import "../css/home.css";
