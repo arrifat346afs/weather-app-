fab6bcc8501dceaea9a8d26787d4b194553d926e
arrifat346afs <arrifat1235@gmail.com>
2025-02-24 10:53:29 +0600

    fix(home): Uncomments fetchWeather function

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import SearchBox from "./SearchBox";
 import WatherCard from "./WatherCard";
 import "../css/home.css";
-import {fetchWeather} from '../services/Api'
 
 const api = {
   weatherKey: "911c12b46fee4dba8c5133138252002",
@@ -56,36 +55,36 @@ const Home = () => {
     );
   }, []);
 
-  // const fetchWeather = (query) => {
-  //   if (!query) {
-  //     setError("Please enter a location.");
-  //     return;
-  //   }
+  const fetchWeather = (query) => {
+    if (!query) {
+      setError("Please enter a location.");
+      return;
+    }
 
-  //   fetch(`${api.weatherUrl}/current.json?key=${api.weatherKey}&q=${query}`)
-  //     .then((res) => {
-  //       if (!res.ok) {
-  //         throw new Error(`HTTP error! Status: ${res.status}`);
-  //       }
-  //       return res.json();
-  //     })
-  //     .then((result) => {
-  //       setWeatherData(result);
-  //       setSearchHistory((prevHistory) => {
-  //         const newHistory = [...prevHistory];
-  //         if (
-  //           !newHistory.some(
-  //             (item) => item.location.name === result.location.name
-  //           )
-  //         ) {
-  //           newHistory.push(result);
-  //         }
-  //         return newHistory;
-  //       });
-  //       setError(null);
-  //     })
-  //     .catch(() => setError("Error fetching weather data. Please try again."));
-  // };
+    fetch(`${api.weatherUrl}/current.json?key=${api.weatherKey}&q=${query}`)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+      })
+      .then((result) => {
+        setWeatherData(result);
+        setSearchHistory((prevHistory) => {
+          const newHistory = [...prevHistory];
+          if (
+            !newHistory.some(
+              (item) => item.location.name === result.location.name
+            )
+          ) {
+            newHistory.push(result);
+          }
+          return newHistory;
+        });
+        setError(null);
+      })
+      .catch(() => setError("Error fetching weather data. Please try again."));
+  };
 
   const searchPress = () => {
     fetchWeather(searchTerm.trim());
