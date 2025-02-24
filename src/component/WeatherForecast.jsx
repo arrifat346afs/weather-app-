bc877cafbbd11bcae66e18e3fd955b19ca26023f
arrifat346afs <arrifat1235@gmail.com>
2025-02-24 11:24:54 +0600

    feat: update 1 files (src/component/WeatherForecast.jsx) at 05:24

--- src/component/WeatherForecast.jsx
+++ src/component/WeatherForecast.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import React, { useEffect } from "react";
 import { useState } from "react";
 const WeatherForecast = ({city}) => {
     const [forecastData, setForecastData] = useState([]);
